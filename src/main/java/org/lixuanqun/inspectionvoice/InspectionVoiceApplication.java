0bd4a96dd591f42db31a0c1fe92505e29ee30745
lixuanqun <793005378@qq.com>
2021-12-23 23:17:06 +0800

    项目初始化

---
+++ src/main/java/org/lixuanqun/inspectionvoice/InspectionVoiceApplication.java
@@ -0,0 +1,13 @@
+package org.lixuanqun.inspectionvoice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InspectionVoiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InspectionVoiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/lixuanqun/inspectionvoice/InspectionVoiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.lixuanqun.inspectionvoice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InspectionVoiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
